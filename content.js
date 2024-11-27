97b50745fb1e9b6383bba13aa8f973ce0534a9b1
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-11-28 05:35:15 +0800

    add this

--- content.js
+++
@@ -1,7 +0,0 @@
-const content = [
-  {
-    titlePage: "51 Years of Excellence MFI Polytechnic",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quibusdam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quibusdam!",
-  },
-];
